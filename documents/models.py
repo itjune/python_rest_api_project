aedb0a8dd4eb95043a35916f1bf53502a3234941
Evgenia Fedoseyeva <>
2022-08-10 23:11:43 +0100

    add todo

--- documents/models.py
+++ documents/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# TODO: need second model to store docs content
+
 class Document(models.Model):
     id = models.AutoField(primary_key=True)
     slug = models.CharField(max_length=50)
